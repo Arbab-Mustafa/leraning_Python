649215249fbe298a7301c0b58a1fd2be8fa6df5a
Arbab-Mustafa <22-se-58@students.uettaxila.edu.pk>
2025-02-07 21:16:29 +0500

    learning python

---
+++ app.py
@@ -0,0 +1,13 @@
+
+# making console based application using python
+import datetime as dt
+from cold import Weather
+
+def main():
+    # month = int(input("Enter month number: "))
+    # Weather(month)
+
+
+if __name__ == "__main__":
+    main()
+             
